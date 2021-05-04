76a3609f4d302bf208c4f1bc357e6e2082569fdd
Farjaux <67696699+Farjaux@users.noreply.github.com>
2021-05-04 08:22:33 -0700

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '78f00b06-f2c4-45bf-ab1d-1257071d98f2';
+export const MODAL_CLOSE_SEC = 2.5;
